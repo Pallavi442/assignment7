2296ba37bf3f2cbd3532b7c6a31d1623b5b14735
Pallavi <patilpallavi1895@gmail.com>
2024-12-19 17:18:42 +0530

    router changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
 import './App.css';
 import NavbarMain from './components/navbar/NavbarMain';
 import PersonalLoan from './components/personalLoan/PersonalLoan';
 
-
 function App() {
   return (
-    <div className="App">
-      <NavbarMain/>
-      <PersonalLoan/>
-    </div>
+    <Router> 
+      <div className="App">
+        <NavbarMain />
+        <Routes>
+          {/* <Route path="/" element={<PersonalLoan />} /> */}
+          <Route path="/loan" element={<PersonalLoan />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/navbar/NavbarMain.js
+++ src/components/navbar/NavbarMain.js
@@ -1,39 +1,52 @@
 import React from 'react';
 import './NavbarMain.css';
-import { FaAngleDown } from "react-icons/fa6";
+import { FaAngleDown } from 'react-icons/fa6';
+import { Link } from 'react-router-dom'; 
 
 function NavbarMain() {
   return (
     <div className='nav-container'>
-        <div className='top-nav'>
-            <ul>
-                <li>Personal |</li>
-                <li>NRI |</li>
-                <li>SME |</li>
-                <li>Wholesale |</li>
-                <li>Agri |</li>
-            </ul>
-
-        </div>
-        <div className='bottom-nav'>
-            <ul>
-                <li>Home <FaAngleDown /></li>
-                <li>Pay <FaAngleDown /></li>
-                <li>Save <FaAngleDown /></li>
-                <li>Invest <FaAngleDown /></li>
-                <li>Loans <FaAngleDown /></li>
-                <li>Insure <FaAngleDown /></li>
-            </ul>
-        </div>
-        <div className='url-container'>
+      <div className='top-nav'>
         <ul>
-                <li>Personal \</li>
-                <li>Loans \</li>
-                <li>Apply for loan \</li>
-            </ul>
-        </div>
+          <li>Personal |</li>
+          <li>NRI |</li>
+          <li>SME |</li>
+          <li>Wholesale |</li>
+          <li>Agri |</li>
+        </ul>
+      </div>
+      <div className='bottom-nav'>
+        <ul>
+          <li>
+            <Link to="/">Home</Link> <FaAngleDown />
+          </li>
+         
+          <li>
+            <Link to="/">Pay</Link> <FaAngleDown />
+          </li>
+          <li>
+            <Link to="/">Save</Link> <FaAngleDown />
+          </li>
+          <li>
+            <Link to="/">Invest</Link> <FaAngleDown />
+          </li>
+          <li>
+          <Link to="/loan">Loan</Link> <FaAngleDown /> 
+          </li>
+          <li>
+            <Link to="/">Insure</Link> <FaAngleDown />
+          </li>
+        </ul>
+      </div>
+      <div className='url-container'>
+        <ul>
+          <li>Personal \</li>
+          <li>Loans \</li>
+          <li>Apply for loan \</li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 
-export default NavbarMain
+export default NavbarMain;
