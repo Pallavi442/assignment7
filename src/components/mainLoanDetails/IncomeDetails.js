2dfcebabc53980b2e2522ff696e882f7c8e7ecfe
Pallavi <patilpallavi1895@gmail.com>
2024-12-24 15:53:47 +0530

    Income Type component initial commit

--- src/components/mainLoanDetails/IncomeDetails.js
+++ src/components/mainLoanDetails/IncomeDetails.js
@@ -7,6 +7,28 @@ function IncomeDetails() {
   return (
     <div className='incomeDetails-Container'>
         <CommonNav activeTab={3}/>
+        <form>
+            <h3>Income Detail</h3>
+            <div className='main-container'>
+                <div className='grp-1'>
+                    <div>
+                    <div>
+                        <label>Income Type</label>
+                        <select
+                            name="inComeType"
+                            // value={formData.maritalStatus}
+                            // onChange={handleInputChange}
+                        >
+                            <option value="">--select Income Type--</option>
+                            <option value="permanent">Permanent</option>
+                            <option value="temparary">Temparary</option>
+                        </select>
+                        {/* {errors.maritalStatus && <small>{errors.maritalStatus}</small>} */}
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </form>
     </div>
   )
 }
